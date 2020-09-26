15b231df653272c427fbdccfa797de50aecfe295
Alfaperro <aedimaria17@gmail.com>
2020-09-26 18:13:03 -0300

    Documentacion interfaces

--- src/miApi/PilaTDA.java
+++ src/miApi/PilaTDA.java
@@ -1,13 +1,53 @@
 package miApi;
 
 public interface PilaTDA {
+	
+	
+	
+	/**
+	 * Agrega un elemento. 
+	 * @precond La estructura debe estar inicializada
+	 * @param x
+	 */
+	
+	
     void apilar(int x);
 
+    
+    
+    
+    /**
+     * Elimina el último elemento agregado. 
+     * @precond La estructura no debe estar vacía
+     */
+    
     void desapilar();
 
+    
+    
+    /**
+     * Obtiene el primer elemento a eliminar (no se elimina). 
+     * @precond La estructura no debe estar vacía
+     * @return elemento a eliminar
+     */
+    
     int tope();
+    
+    
+    
+    /**
+     *  Indica si la pila no contiene elementos.
+     *  @precond La estructura debe estar inicializada.
+     *  @return Boolean
+     */
 
     boolean pilaVacia();
-
+ 
+       
+    
+    
+    /**
+     * Inicializa la estructura pila.
+     */
     void inicializarPila();
 }
